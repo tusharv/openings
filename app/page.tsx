0faee88d6bb1cc4e3ee0e633833235cedd6d3bca
Tushar Vaghela <tushar.proximity@hotstar.com>
2023-11-29 18:56:23 +0530

    feat: added support for google tag manager

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 'use client';
-
-import Container from './components/Container';
+import Script from "next/script";
+import Container from "./components/Container";
 
 export default function Home() {
   return (
     <main>
+      <Script id="google-analytics">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W67MJ95D');
+          `}
+      </Script>
       <Container />
     </main>
   );
